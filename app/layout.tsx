74d9779b3cad853ed4ceaf3ebb4d5f01a5c8cfa7
@ryannanda2001-gif <ryan.nanda2001@gmail.com>
2026-03-19 23:40:29 +0700

    ganti judul

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Universal Computer",
-  description: "Universal Computer",
+  description: "Toko Computer",
 };
 
 export default function RootLayout({
